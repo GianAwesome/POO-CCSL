db0b22ac148cec4d2dc4b855006524eede2b2c43
HugoMitsumori <hugo.mitsumori@gmail.com>
2016-04-10 19:38:23 -0300

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,29 @@ ActiveRecord::Base.transaction do
     EventType.create("name" => event_name)
   end
 end
+
+events = [
+  {:title => 'Evento 1', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-03-01', :time=>'03:25:00' },
+    {:title => 'Evento 2', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-02-01', :time=>'03:24:00' },
+    {:title => 'Evento 3', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-01-01', :time=>'00:25:00' },
+    {:title => 'Evento 4', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-03-24', :time=>'03:25:00' },
+    {:title => 'Evento 5', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-02-01', :time=>'03:25:00' },
+    {:title => 'Evento 6', :summary=>'um evento teste', :place => 'Auditorio do CCSL', 
+    :address => 'perto do IME', :speaker => 'Junior Barreira' , :responsible => 'todos' , 
+    :performance => 'huehuheuhe', :user_id=>1, :date=>'2016-01-01', :time=>'03:25:00' }
+
+]
+
+events.each do |event|
+  Event.create!(event)
+end
