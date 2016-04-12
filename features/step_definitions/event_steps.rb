a0448e0462a3be88f6743d4ec26f9b58484b700c
Gian <gian_l.cicca@hotmail.com>
2016-04-11 22:22:32 -0300

    Further Create Event tests

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -2,12 +2,6 @@ Then /the event "(.+)" exists/ do |eventname|
   Event.exists?(name: eventname)
 end
 
-Given(/^the following event_type exists:$/) do |event_type_table|
-	event_type_table.hashes.each do |event_type|
-		EventType.create(event_type)
-	end
-end
-
 Given(/^the following events exists:$/) do |events_table|
 	events_table.hashes.each do |event|
 		Event.create(event)
